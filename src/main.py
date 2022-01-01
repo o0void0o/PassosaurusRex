0d575185f3c96daba304c195a8e32b2faf1016f1
Etienne <etienne.swanepoel2@gmail.com>
2022-01-01 11:37:03 +0200

    added some more meat....

--- src/main.py
+++ src/main.py
@@ -16,9 +16,6 @@ def loadDatabase():
 		with open("database.json", "r") as read_file:
 			print("Converting JSON encoded data into Python dictionary")
 			x = json.load(read_file)
-			
-	
-	
 		return x
 	except Exception as e: 
 		print(e)
@@ -85,12 +82,17 @@ def promptForUsername():
 	return input('Enter your username:')
 
 def promptForManualGeneratedPassword():
-	return input('Enter your new password:')
+	os.system('clear')
+	return input('Enter your new manual password:')
+
+def promptForManualGeneratedPasswordConfirm():
+	os.system('clear')
+	return input('Confirm manual password:')
+
 
 def validateMasterPassword(password):
 	print(database)
-	return password==database['masterPW']
-
+	assert password==database['masterPW']
 
 def promptForNewMasterPassword():
 	os.system('clear')
@@ -107,8 +109,8 @@ def save_data(json_data):
 
 def handleMainMenu():
 	os.system('clear')
-	print("*DEBUG***=-------"+database['username'])
-	print("***********MainMenu***********")
+
+	print("*********** Main Menu ***********")
 	print("1) Password Vault")
 	print("2) Change Master Password")
 	print("3) Exit")
@@ -133,22 +135,52 @@ def handlePasswordVault():
 
 	option=input()
 	if(option == '1'):
-		handleSecurePasswords()
+		handleSecurePasswordsMenu()
 	if(option=='2'):
-		addNewPassword()
+		addNewSecuredPassword()
 	if(option=='3'):
 		handleMainMenu()
 
+def addNewSecuredPassword():
+	passwordAlias = promptForAlias()
+	pw = handleNewPasswordMenu(passwordAlias,'')
 
-def addNewPassword():
-	os.system('clear')
-	passwordAlias=input("Enter the password alias: ")
-	os.system('clear')
-	handleNewPasswordMenu(passwordAlias)
+	newPW =  SecuredPassword(passwordAlias,pw)
+	database['securedPasswords'].append(newPW.toJson())
+
+	print(database)
+	save_data(database) # saving the json file
+
+	handleSavedNewSecuredPasswordMenu(passwordAlias)
 
 
-def handleNewPasswordMenu(passwordAlias):
+
+def handleSavedNewSecuredPasswordMenu(passwordAlias):
 	os.system('clear')
+	print("***********"+passwordAlias +" saved***********")
+	print("1) View Secueurd passwords")
+	print("2) Main Menu")
+
+	option=input()
+	if(option == '1'):
+		handleSecurePasswordsMenu()
+	else:
+		handleMainMenu()
+
+	
+def promptForAlias():
+	os.system('clear')
+	return input("Enter the password alias: ")	
+
+
+def handleNewPasswordMenu(passwordAlias,mesg):
+	os.system('clear')
+	if mesg!='':
+		print("****************************")
+		print(mesg)
+		print("****************************")
+
+	
 	print("1) Genereate Password")
 	print("2) Manual Password")
 	print("3) Back")
@@ -158,36 +190,42 @@ def handleNewPasswordMenu(passwordAlias):
 		print("dfg")
 	if(option=='2'):
 		pw=promptForManualGeneratedPassword()
+		pw2=promptForManualGeneratedPasswordConfirm()
+
+		if pw !=pw2:
+			handleNewPasswordMenu(passwordAlias,"Provdided passwords dont match")
+
 	if(option=='3'):
 		handleMainMenu()
-	newPW=SecuredPassword(passwordAlias,pw)
+
+	return pw
 
 
-	database['securedPasswords'].append(newPW.toJson())
-
-	print(database)
-	save_data(database) # saving the json file
-	##thelist[]
+	
 
 
 
-def handleSecurePasswords():
+def handleSecurePasswordsMenu():
 	os.system('clear')
-	print("***********SecuredPasswords***********")
+	print("*********** Secured Passwords ***********")
 	
 	for idx,item in enumerate(database["securedPasswords"]):
 		daSON=json.loads(item)
 		print(str(idx)+") "+daSON['alias'])
 
-	print("6) Back")
+	print("")
+	print("x) Main Menu")
+
 
 	option=input()
+
+	if(option=='x'):
+		handlePasswordVault()
+
+
 	handleShowSecurePassword(option)
 
-	if(option=='6'):
-		handlePasswordVault()
-	
-	
+
 
 def handleShowSecurePassword(number):
 	os.system('clear')
@@ -202,7 +240,7 @@ def handleShowSecurePassword(number):
 	option=input()
 
 	if(option=='3'):
-		handleSecurePasswords()
+		handleSecurePasswordsMenu()
 
 
 if __name__ == "__main__":
